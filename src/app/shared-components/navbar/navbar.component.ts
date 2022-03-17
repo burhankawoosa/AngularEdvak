6eeb413aa80b7e49c303b782f0519ac45ed130c6
burhan kawoosa <kawoosaburhan@gmail.com>
2022-03-17 15:24:45 +0530

    17032022

--- src/app/shared-components/navbar/navbar.component.ts
+++ src/app/shared-components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+@Output() public sidenavToggle= new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
   }
-public onToggleSidenav=() =>{}
+public onToggleSidenav=() =>{
+  this.sidenavToggle.emit();
+}
 }
